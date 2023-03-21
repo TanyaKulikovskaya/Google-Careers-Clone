2075ec10682bb267ce184f0547e47a2b971e9b62
Tanya Kulikovskaya <tatsiana@pedanov.ru>
2023-03-21 12:12:50 +0300

    job view

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,18 +1,70 @@
 <template>
-  <h1 class="font-bold text-6xl tracking-tight py-10">
-    Job View for job {{ currentJobId }}
-  </h1>
+  <section class="py-6">
+    <h1 class="mb-2 text-4xl text-gray-600">
+      {{ currentJob.title }}
+    </h1>
+
+    <ul class="border-b border-solid border-gray-100">
+      <li
+        v-for="(location, index) in currentJob.locations"
+        :key="index"
+        class="pr-2 inline-block"
+      >
+        {{ location }}
+      </li>
+    </ul>
+
+    <h2 class="my-4 text-lg uppercase text-gray-600">
+      Minimum qualifications:
+    </h2>
+    <ul class="list-disc pl-8">
+      <li
+        v-for="(qualification, index) in currentJob.minimumQualifications"
+        :key="index"
+        class="text-sm"
+      >
+        {{ qualification }}
+      </li>
+    </ul>
+    <h2 class="my-4 text-lg uppercase text-gray-600">
+      Preferred qualifications:
+    </h2>
+    <ul class="list-disc pl-8">
+      <li
+        v-for="(qualification, index) in currentJob.preferredQualifications"
+        :key="index"
+        class="text-sm"
+      >
+        {{ qualification }}
+      </li>
+    </ul>
+    <h2 class="my-4 text-lg uppercase text-gray-600">About the job</h2>
+    <p
+      v-for="(description, index) in currentJob.description"
+      :key="index"
+      class="text-sm"
+    >
+      {{ description }}
+    </p>
+  </section>
 </template>
 
 <script>
+import { mapState } from 'pinia'
+import { useJobsStore } from '@/stores/jobs.js'
+
 export default {
   name: 'JobView',
   computed: {
+    ...mapState(useJobsStore, {
+      jobs: 'jobs',
+      currentJob() {
+        return this.jobs?.filter((job) => job.id == this.currentJobId)[0]
+      },
+    }),
     currentJobId() {
       return this.$route.params.id
     },
   },
 }
 </script>
-
-<style></style>
