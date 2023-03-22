c01931c88669fbb80b75b869b017babf8d61dc5b
Tanya Kulikovskaya <tatsiana@pedanov.ru>
2023-03-22 11:43:59 +0300

    spotlights component

---
+++ src/components/Hero/SpotLights.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <li v-for="spot in spots" :key="spot.id">
+      <slot :spotlight="spot"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'SpotLights',
+  data() {
+    return {
+      spots: [],
+    }
+  },
+  mounted() {
+    this.fetchSpots()
+  },
+  methods: {
+    async fetchSpots() {
+      const { data } = await axios.get('http://localhost:3000/spotlights')
+      this.spots = data
+    },
+  },
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,35 @@
 <template>
   <hero-section />
+  <spot-lights class="flex flex-row flex-wrap py-8">
+    <template #default="slotProps">
+      <router-link
+        to="/job/results"
+        class="m-4 flex flex-col rounded-lg bg-neutral-100 transform transition duration-300 hover:scale-105"
+      >
+        <img
+          :src="slotProps.spotlight.img"
+          class="h-64 rounded-tl-lg rounded-tr-lg"
+        />
+        <div class="mt-3 h-32 px-6 py-4">
+          <h4 class="text-md mb-3 text-sky-500">
+            {{ slotProps.spotlight.title }}
+          </h4>
+          <p>{{ slotProps.spotlight.description }}</p>
+        </div>
+      </router-link>
+    </template>
+  </spot-lights>
 </template>
 
 <script>
 import HeroSection from '@/components/Hero/HeroSection.vue'
+import SpotLights from '@/components/Hero/SpotLights.vue'
+
 export default {
   name: 'HomeView',
   components: {
     HeroSection,
+    SpotLights,
   },
 }
 </script>
-
-<style></style>
