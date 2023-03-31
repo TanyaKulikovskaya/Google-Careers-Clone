d06a2655aaeb6a8e16aac6fa9b51c3bcaf22ff77
Tanya Kulikovskaya <tatsiana@pedanov.ru>
2023-03-31 09:57:00 +0300

    add typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,21 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', 'prettier'],
+
   parserOptions: {
-    parser: '@babel/eslint-parser',
+    parser: '@typescript-eslint/parser',
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
+
   overrides: [
     {
       files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
@@ -19,4 +24,11 @@ module.exports = {
       },
     },
   ],
+
+  'extends': [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    'prettier',
+    '@vue/typescript'
+  ]
 }
